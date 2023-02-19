6a39e3d7294c5ebae97fa0e707af8ac58f70ae28
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-20 00:13:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MyBotzz/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/Azanpopz/rashmika-language.git /Azanpopz
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /Azanpopz
 fi
-cd /DQ-The-File-Donor
+cd /Azanpopz
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting Azanpopz...."
 python3 bot.py
